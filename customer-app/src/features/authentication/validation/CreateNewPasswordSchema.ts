a70449c0846fa85ae163a6ad86ea43c2643b72c6
vbudithi <vivek.budithi@gmail.com>
2026-08-06 22:42:10 +1000

    feat: add new password UI and reset success UI

---
+++ customer-app/src/features/authentication/validation/CreateNewPasswordSchema.ts
@@ -0,0 +1,20 @@
+
+import { z } from "zod";
+
+export const CreateNewPasswordSchema = z
+    .object({
+        password: z
+            .string()
+            .min(8, "Password must be at least 8 characters")
+            .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+            .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+            .regex(/[0-9]/, "Password must contain at least one number"),
+
+        confirmPassword: z.string(),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+        path: ["confirmPassword"],
+        message: "Passwords do not match"
+    });
+
+export type CreateNewPasswordFormData = z.infer<typeof CreateNewPasswordSchema>;
